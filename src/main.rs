839fa642ed7493af5268c199e65649e60d2512ad
mo <lol>
2024-06-17 20:44:35 +0100

    yo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{sync::{mpsc::{self, Receiver, Sender}, Arc, Mutex}};
-use wgpu::{core::device, util::DeviceExt};
+use wgpu::{core::device, util::{DeviceExt, RenderEncoder}};
 use winit::{
     event::{self, *},
     event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
@@ -103,10 +103,33 @@ struct Vertex {
     color: [f32; 3],
 }
 
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS
+        }
+    }
+}
+
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0]},
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0]},
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 struct GraphicEngine<'a> {
@@ -118,6 +141,9 @@ struct GraphicEngine<'a> {
     window: &'a Window,
     pipeline: wgpu::RenderPipeline,
     buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+    num_vertices: u32, 
 }
 
 impl<'a> GraphicEngine<'a> {
@@ -192,7 +218,9 @@ impl<'a> GraphicEngine<'a> {
                 vertex: wgpu::VertexState {
                     module: &shader,
                     entry_point: "vs_main",
-                    buffers: &[]
+                    buffers: &[
+                        Vertex::desc(),
+                    ]
                 },
                 fragment: Some(wgpu::FragmentState { 
                     module: &shader, 
@@ -230,7 +258,18 @@ impl<'a> GraphicEngine<'a> {
                 usage: wgpu::BufferUsages::VERTEX
             }
         );
-            
+
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+
+        let num_vertices = VERTICES.len() as u32;
+        let num_indices = INDICES.len() as u32;
+
         return GraphicEngine {
             surface,
             device,
@@ -239,7 +278,10 @@ impl<'a> GraphicEngine<'a> {
             size,
             window,
             pipeline,
-            buffer
+            buffer,
+            index_buffer,
+            num_indices,
+            num_vertices,
         };
     }
     
@@ -251,6 +293,8 @@ impl<'a> GraphicEngine<'a> {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+
+            use image::GenericImageView;
         }
     }
 
@@ -296,13 +340,14 @@ impl<'a> GraphicEngine<'a> {
                 occlusion_query_set: None,
                 timestamp_writes: None
             });
-
             render_pass.set_pipeline(&self.pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_vertex_buffer(0, self.buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
+        output.present();;
 
         Ok(())
     }
